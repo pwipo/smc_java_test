51435c3835979f9e866ba5dca83728d12dab9212
pwipo <pwipo.seits@gmail.com>
2024-05-08 14:49:40 +0300

    update

--- src/main/java/ru/smcsystem/test/emulate/ExecutionContext.java
+++ src/main/java/ru/smcsystem/test/emulate/ExecutionContext.java
@@ -1,23 +1,23 @@
 package ru.smcsystem.test.emulate;
 
-import ru.smcsystem.api.dto.*;
+import ru.smcsystem.api.dto.IConfiguration;
+import ru.smcsystem.api.dto.IExecutionContext;
+import ru.smcsystem.api.dto.IExecutionContextManaged;
+import ru.smcsystem.api.dto.ISourceManaged;
 import ru.smcsystem.api.enumeration.MessageType;
-import ru.smcsystem.api.enumeration.SourceGetType;
 import ru.smcsystem.api.exceptions.ModuleException;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class ExecutionContext implements IExecutionContextManaged {
+public class ExecutionContext extends SourceList implements IExecutionContextManaged {
     private ExecutionContextToolImpl executionContextTool;
     private Configuration configuration;
     private String name;
     private List<IExecutionContext> executionContexts;
     private List<IConfiguration> managedConfigurations;
-    private SourceList sourceList;
+    // private SourceList sourceList;
     private Integer maxWorkInterval;
     private Boolean enable;
     private String type;
@@ -32,12 +32,13 @@ public class ExecutionContext implements IExecutionContextManaged {
             , Integer maxWorkInterval
             , String type
     ) {
+        super(executionContextTool, configuration.getName(), name, sources != null ? new ArrayList<>(sources) : new ArrayList<>());
         this.executionContextTool = executionContextTool;
         this.configuration = configuration;
         this.name = name;
         this.executionContexts = executionContexts != null ? new ArrayList<>(executionContexts) : new ArrayList<>();
         this.managedConfigurations = managedConfigurations != null ? new ArrayList<>(managedConfigurations) : new ArrayList<>();
-        this.sourceList = new SourceList(executionContextTool, configuration.getName(), name, sources != null ? new ArrayList<>(sources) : new ArrayList<>());
+        // this.sourceList = new SourceList(executionContextTool, configuration.getName(), name, sources != null ? new ArrayList<>(sources) : new ArrayList<>());
         this.maxWorkInterval = maxWorkInterval != null ? maxWorkInterval : -1;
         this.enable = true;
         this.type = type != null ? type : "default";
@@ -150,193 +151,6 @@ public class ExecutionContext implements IExecutionContextManaged {
     }
 
     @Override
-    public int countSource() {
-        return sourceList.countSource();
-    }
-
-    @Override
-    public Optional<ISource> getSource(int id) {
-        return sourceList.getSource(id);
-    }
-
-    @Override
-    public ISourceManaged createSource(IConfiguration configuration, SourceGetType getType, int countLast, boolean eventDriven) {
-        return sourceList.createSource(configuration, getType, countLast, eventDriven);
-    }
-
-    @Override
-    public ISourceManaged createSource(IExecutionContext executionContext, SourceGetType getType, int countLast, boolean eventDriven) {
-        return sourceList.createSource(executionContext, getType, countLast, eventDriven);
-    }
-
-    @Override
-    public ISourceManaged createSource(String value) {
-        return sourceList.createSource(value);
-    }
-
-    @Override
-    public ISourceManaged createSource(Byte value) {
-        return sourceList.createSource(value);
-    }
-
-    @Override
-    public ISourceManaged createSource(Short value) {
-        return sourceList.createSource(value);
-    }
-
-    @Override
-    public ISourceManaged createSource(Integer value) {
-        return sourceList.createSource(value);
-    }
-
-    @Override
-    public ISourceManaged createSource(Long value) {
-        return sourceList.createSource(value);
-    }
-
-    @Override
-    public ISourceManaged createSource(Float value) {
-        return sourceList.createSource(value);
-    }
-
-    @Override
-    public ISourceManaged createSource(Double value) {
-        return sourceList.createSource(value);
-    }
-
-    @Override
-    public ISourceManaged createSource(BigInteger value) {
-        return sourceList.createSource(value);
-    }
-
-    @Override
-    public ISourceManaged createSource(BigDecimal value) {
-        return sourceList.createSource(value);
-    }
-
-    @Override
-    public ISourceManaged createSource(byte[] value) {
-        return sourceList.createSource(value);
-    }
-
-    @Override
-    public ISourceManaged createSource(Boolean value) {
-        return sourceList.createSource(value);
-    }
-
-    @Override
-    public ISourceManaged createSource(ObjectArray value) {
-        return sourceList.createSource(value);
-    }
-
-    @Override
-    public ISourceManaged createSource() {
-        return sourceList.createSource();
-    }
-
-    @Override
-    public ISourceManaged createSource(ObjectArray value, List<String> fields) {
-        return sourceList.createSource(value, fields);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, IConfiguration configuration, SourceGetType getType, int countLast, boolean eventDriven) {
-        return sourceList.updateSource(id, configuration, getType, countLast, eventDriven);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, IExecutionContext executionContext, SourceGetType getType, int countLast, boolean eventDriven) {
-        return sourceList.updateSource(id, executionContext, getType, countLast, eventDriven);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, String value) {
-        return sourceList.updateSource(id, value);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, Byte value) {
-        return sourceList.updateSource(id, value);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, Short value) {
-        return sourceList.updateSource(id, value);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, Integer value) {
-        return sourceList.updateSource(id, value);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, Long value) {
-        return sourceList.updateSource(id, value);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, Float value) {
-        return sourceList.updateSource(id, value);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, Double value) {
-        return sourceList.updateSource(id, value);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, BigInteger value) {
-        return sourceList.updateSource(id, value);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, BigDecimal value) {
-        return sourceList.updateSource(id, value);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, byte[] value) {
-        return sourceList.updateSource(id, value);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, Boolean value) {
-        return sourceList.updateSource(id, value);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, ObjectArray value) {
-        return sourceList.updateSource(id, value);
-    }
-
-    @Override
-    public ISourceManaged updateSource(int id, ObjectArray value, List<String> fields) {
-        return sourceList.updateSource(id, value, fields);
-    }
-
-    /*
-    @Override
-    public void changeOrderUp(int id) {
-        sourceList.changeOrderUp(id);
-    }
-
-    @Override
-    public void changeOrderDown(int id) {
-        sourceList.changeOrderDown(id);
-    }
-    */
-
-    @Override
-    public Optional<ISourceManaged> getSourceManaged(int id) {
-        return sourceList.getSourceManaged(id);
-    }
-
-    @Override
-    public void removeSource(int id) {
-        sourceList.removeSource(id);
-    }
-
-    @Override
     public IConfiguration getConfiguration() {
         return configuration;
     }
@@ -359,11 +173,6 @@ public class ExecutionContext implements IExecutionContextManaged {
     @Override
     public boolean isActive() {
         return false;
-    }
-
-    @Override
-    public Optional<ISourceListManaged> getSourceListManaged(int id) {
-        return sourceList.getSourceListManaged(id);
     }
 
     @Override
